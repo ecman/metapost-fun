fcb9911b04c2f6dbd359043e5bc2e8c5213fa700
ecman <ecman@users.noreply.github.com>
2017-05-13 10:55:40 -0700

    Add DEBUG flag; listen for error event

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 const http = require('http');
+const DEBUG = false;
 const server = http.createServer((req, res) => {
+  let body;
   req.setEncoding('utf8');
-  req.on('data', (data) => {});
+  req.on('data', (data) => body += data);
   req.on('end', () => {
-    console.log(`Responding to: ${req.socket.address().address}`);
+    console.log(`Responding to: ${req.socket.remoteAddress}`);
+    if (DEBUG) {
+      console.log(JSON.stringify(req.headers));
+      console.log('REQUEST BODY ----');
+      console.log(body);
+      console.log('END REQUEST BODY ----');
+    }
     res.writeHead(200, {'Content-Type':'text/plain'});
     res.end('Thanks!');
   });
-}).listen(3000, '127.0.0.1', () => console.log('Listening'));
+})
+.on('error', (err) => 
+  console.log(
+    `Error: ${err.message} Code: ${err.code}`)
+)
+.listen(3000, '127.0.0.1', () => console.log('Listening'));
