bbf68891199777bbb130585d8e85686895e1a4f4
ecman <ecman@users.noreply.github.com>
2017-05-13 01:58:12 -0700

    Make server log requests to stdout

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type':'text/plain'});
-  res.end('Thanks!');
-}).listen(3000);
+  req.setEncoding('utf8');
+  req.on('data', (data) => {});
+  req.on('end', () => {
+    console.log(`Responding to: ${req.socket.address().address}`);
+    res.writeHead(200, {'Content-Type':'text/plain'});
+    res.end('Thanks!');
+  });
+}).listen(3000, '127.0.0.1', () => console.log('Listening'));
